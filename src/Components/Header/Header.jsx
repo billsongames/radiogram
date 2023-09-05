77caebd0d914faa2174611d5a827b1378f30a73b
Paul Spencer <billsongames@gmail.com>
2023-09-05 14:18:47 +0100

    added header component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,11 +3,20 @@ import React from "react";
 import Title from "./Title";
 import Login from "./Login";
 
+import "./header.css"
+
 const Header = () => {
   return(
-    <div>
-      <Title/>
-      <Login/>
+    <div className="header-container">
+      <div className="title-container">
+        <h1>
+          RadioPlayer
+        </h1>
+      </div>
+      <div className="login-container">
+        <Login/>
+      </div>
+      
     </div>
   )
 }
